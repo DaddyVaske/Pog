703eab18dd1cb3af78206c4cae59f337abc3ce15
DaddyVaske <vasilije.stankovic2005@gmail.com>
2024-10-06 16:58:45 +0200

    Smekeri Update

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
 
-        Smeker vaske= new Smeker("Vaske");
-        Smeker pera= new Smeker("Pero");
 
-        vaske.resiSmeker();
+
 
     }
 }
