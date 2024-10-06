aa333b00b9a1a7d8c08e694d9132c035ffd39cf0
DaddyVaske <vasilije.stankovic2005@gmail.com>
2024-10-06 17:07:53 +0200

    Smekeri Update

---
+++ src/FrontEnd/MainFrame.java
@@ -0,0 +1,16 @@
+package FrontEnd;
+
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame(String text){
+
+        this.setSize(1280,720);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
